76b3f68fef5267b98a1b0c393926146306d88ea2
Reid Weber <reid_weber@hotmail.com>
2014-11-03 11:51:45 -0800

    Added Tab Bar Controller and Map View

---
+++ MyLocations/LocationListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LocationListViewController.swift
+//  MyLocations
+//
+//  Created by Reid Weber on 11/3/14.
+//  Copyright (c) 2014 Reid Weber. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class LocationListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyLocations/MapViewController.swift
@@ -0,0 +1,75 @@
+//
+//  MapViewController.swift
+//  MyLocations
+//
+//  Created by Reid Weber on 11/3/14.
+//  Copyright (c) 2014 Reid Weber. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.locationManager.delegate = self
+        self.mapView.delegate = self
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: "didLongPressMap:")
+        self.mapView.addGestureRecognizer(longPress)
+        
+        switch CLLocationManager.authorizationStatus() as CLAuthorizationStatus {
+        case .Authorized:
+            println("Authorized")
+            //self.locationManager.startUpdatingLocation()
+            self.mapView.showsUserLocation = true
+        case .NotDetermined:
+            println("Not Determined")
+            // Need to request Auth and add keys to info.plist
+            self.locationManager.requestAlwaysAuthorization()
+        case .Denied:
+            println("Denied")
+        case .Restricted:
+            println("Restricted")
+        default:
+            println("Default")
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func didLongPressMap(sender: UILongPressGestureRecognizer) {
+        println("Long Press!")
+    }
+
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "ANNOTATION")
+        annotationView.animatesDrop = true
+        annotationView.canShowCallout = true
+        let addButton = UIButton.buttonWithType(UIButtonType.ContactAdd) as UIButton
+        annotationView.rightCalloutAccessoryView = addButton
+        return annotationView
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
